4d1b74f825905fd551c97b040c5624f6eb4fcb60
ManuDev03 <manuprajan03@gmail.com>
2021-07-23 19:38:35 +0530

    adding fontawesome icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faThumbsDown,faAddressBook,faBell} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mypro';
+  bell:any = faBell
+  thumbsdown:any = faThumbsDown
+  address:any = faAddressBook
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule} from '@fortawesome/angular-fontawesome'
 import { CalcAppComponent } from './calc-app/calc-app.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { CalcAppComponent } from './calc-app/calc-app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
